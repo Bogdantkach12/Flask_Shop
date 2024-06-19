7211abd53228e68b388716ae76fc460267aba7bf
Bogdantkach12 <121688819+Bogdantkach12@users.noreply.github.com>
2024-06-19 13:57:25 +0300

    Add files via upload

--- Flask_Shop/registration_page/app.py
+++ Flask_Shop/registration_page/app.py
@@ -1,1 +1,8 @@
+import flask
 
+registration_page = flask.Blueprint(
+    name = "registration",
+    import_name = "app",
+    static_folder = "static/registration_page",
+    template_folder = "registration_page/templates"
+)
